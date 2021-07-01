0437e17ed227a84ca181a7fc301b661e60ff0f05
Henrique Vitoi <hvitoi@gmail.com>
2021-07-01 18:36:10 -0300

    updates

--- concepts/loops/forof.js
+++ concepts/loops/forof.js
@@ -29,3 +29,48 @@ for (let letter of phrase) {
 }
 
 // Obs: Objects are not interable in forof!!
+
+/**
+ *
+ *
+ *
+ *
+ */
+const returnHaha = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('haha');
+  }, 1000);
+});
+const returnHehe = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('hehe');
+  }, 1000);
+});
+const returnHihi = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('hihi');
+  }, 1000);
+});
+const laughPromises = [returnHaha, returnHehe, returnHihi];
+
+// ---
+
+const findHehe = async () => {
+  let myLaugh;
+  for (let laughPromise of laughPromises) {
+    const res = await laughPromise;
+    if (res === 'hehe') {
+      myLaugh = res;
+      break;
+    }
+  }
+  if (myLaugh) {
+    return myLaugh;
+  } else {
+    return await laughPromises[0];
+  }
+};
+
+// ---
+
+(async () => console.log(await findHehe()))();
