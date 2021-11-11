25b8b6d7580fe4a75279d783f2f523baafe0e340
Henrique Vitoi <hvitoi@gmail.com>
2021-11-11 16:23:01 -0300

    updates

--- basics/methods/js/Array/Array.from.js
+++ basics/methods/js/Array/Array.from.js
@@ -7,5 +7,11 @@ const arr = [...Array(10).keys()];
 //=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 Array.from({ length: 10 }, (_, i) => i + 1);
 
-// From Set
-const arrFromSet = Array.from(new Set([3, 2, 1]));
+// ---
+const arrFromArray = Array.from([1, 2, 3]);
+const arrFromSet = Array.from(new Set([1, 2, 3]));
+const arrFromString = Array.from("foo");
+
+console.log(arrFromSet);
+console.log(arrFromArray);
+console.log(arrFromString);
